84ab87f03fe73e9a35355c2b7b442efe4cda8db7
optimus-alfaomega <a-and-o-technologies@outlook.com>
2026-09-05 21:25:43 -0500

    Se añaden funciones de suma,resta,multiplicacion y division

--- EjemploGit/src/Persona.java
+++ EjemploGit/src/Persona.java
@@ -9,4 +9,19 @@ public class Persona {
         return a + b;
     }
 
+    public static int restar(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static int dividir(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("No se puede dividir por cero");
+        }
+        return a / b;
+    }
+
 }
